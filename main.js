7fd7a198e82b882bc8ac112cbea37cfbac6bc5e6
Tusti-Gupta <86391913+Tusti-Gupta@users.noreply.github.com>
2023-08-15 11:50:47 +0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,13 @@
+function preload() {
+}
+
+function setup() {
+    canvas = createCanvas(500,500);
+    canvas.center();
+    video = createCapture(VIDEO);
+    video.size(500, 500);
+    video.hide();
+}
+
+function draw() {
+}
