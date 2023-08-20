4a3b79c7d3c82f42b9f59bbf1eb30b205b15d36f
Tusti-Gupta <86391913+Tusti-Gupta@users.noreply.github.com>
2023-08-20 17:02:00 +0800

    Add files via upload

--- main.js
+++ main.js
@@ -1,11 +1,18 @@
+noseX = 0;
+noseY = 0;
+myDownload = "";
+
 function preload() {
+  lollipop = loadImage("https://i.postimg.cc/wxbs1CPN/swirl.png");
 }
 
 function setup() {
-    canvas = createCanvas(300, 300);
+    canvas = createCanvas(400, 400);
+    canvas.id("myCanvas"); 
+    snappedImg = document.getElementById("myCanvas"); 
     canvas.center();
     video = createCapture(VIDEO);
-    video.size(300, 300);
+    video.size(400, 400);
     video.hide();
   
     poseNet = ml5.poseNet(video, modelLoaded);
@@ -27,5 +34,11 @@ function gotPoses(results)
 }
 
 function draw() {
-    image(video, 0, 0, 300, 300);
+    image(video, 0, 0, 400, 400);
+    image(lollipop, noseX - 15, noseY - 10, 30, 30);
+}
+
+function snap() {
+  document.getElementById("canvasImg").src = snappedImg.toDataURL();
+  document.getElementById("downloadLink").href = snappedImg.toDataURL();
 }
